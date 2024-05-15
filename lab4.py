b24d84378619bea7447e6c39c18999554461dad8
Error <egor.gmag@yandex.ru>
2024-05-15 08:51:36 +0700

    fixed errors

--- lab4.py
+++ lab4.py
@@ -27,14 +27,14 @@ class SensorCam:
     def __init__(self, camera_name, resolution):
         self._cap = cv2.VideoCapture(camera_name)
         if not self._cap.isOpened():
-            raise Exception('Failed to open camera (wrong index).')
-        self._cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
-        self._cap.set(cv2.CAP_PROP_FRAME_HEIGHT,resolution[1])
-        self._cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
+            raise Exception(f'Failed to open camera (wrong index {camera_name}).')
+        self._camera_name = camera_name
+        self._resolution = resolution
         
     def get(self):
         ret, frame = self._cap.read()
-        return [ret, frame]
+        frame = cv2.resize(frame, self._resolution)
+        return frame
 
     def __del__(self):
         self._cap.release()
@@ -50,21 +50,20 @@ class WindowImage:
     def __del__(self):
         cv2.destroyAllWindows()
 
-def push_to_queue(sensor, queue: Queue, delay):
+def push_to_queue(sensor, queue: Queue):
 
     while True:
         if queue.full():
-            _ = queue.get_nowait()
+            queue.get_nowait()
         else:
             queue.put_nowait(sensor.get())
-            time.sleep(delay)
 
 if __name__ == '__main__':
     logging.basicConfig(filename="./logs/errors.log", level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
     parser = argparse.ArgumentParser(description='parameters')
     parser.add_argument('--camera_index', type=int, default=0)
     parser.add_argument('--resolution', type=str, default='1280x720')
-    parser.add_argument('--frequency', type=float, default=100)
+    parser.add_argument('--frequency', type=float, default=60)
 
     args = parser.parse_args()
 
@@ -79,22 +78,18 @@ if __name__ == '__main__':
     sensor1 = SensorX(0.1)
     sensor2 = SensorX(1)
 
-    queue0 = Queue(2)
-    queue1 = Queue(2)
-    queue2 = Queue(2)
-    queue_video = Queue(2)
-
+    queue0 = Queue(10)
+    queue1 = Queue(10)
+    queue2 = Queue(10)
     
-    thread_sensor0 = threading.Thread(target=push_to_queue, args=(sensor0, queue0, 0), daemon=True)
-    thread_sensor1 = threading.Thread(target=push_to_queue, args=(sensor1, queue1, 0), daemon=True)
-    thread_sensor2 = threading.Thread(target=push_to_queue, args=(sensor2, queue2, 0), daemon=True)
-    thread_video = threading.Thread(target=push_to_queue, args=(camera_sensor, queue_video, delay), daemon=True)
+    thread_sensor0 = threading.Thread(target=push_to_queue, args=(sensor0, queue0), daemon=True)
+    thread_sensor1 = threading.Thread(target=push_to_queue, args=(sensor1, queue1), daemon=True)
+    thread_sensor2 = threading.Thread(target=push_to_queue, args=(sensor2, queue2), daemon=True)
     
     
     thread_sensor0.start()
     thread_sensor1.start()
     thread_sensor2.start()
-    thread_video.start()
 
     image_window = WindowImage(delay)
 
@@ -106,13 +101,10 @@ if __name__ == '__main__':
 
     while not (cv2.waitKey(1) & 0xFF == ord('q')):
         try:
-            if not queue_video.empty():
-                frame = queue_video.get_nowait()
-                ret = frame[0]
-                frame = frame[1]
-                if frame is None:
-                    logging.error(Exception('Unable to read the input.'))
-                    exit(1)
+            frame = camera_sensor.get()
+            if frame is None:
+                logging.error(Exception('Unable to read the input.'))
+                exit(1)
 
             if not queue0.empty():
                 sensor_data0 = queue0.get_nowait()
@@ -132,4 +124,4 @@ if __name__ == '__main__':
                 cv2.putText(frame_copy, 'Sensor2: '+ str(sensor_data2), (frame_copy.shape[1] - 250, frame_copy.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
                 image_window.show("lab4", frame_copy)
         except Exception as out:
-            logging.error(f'Camera out {str(out)}')
+            logging.error(f'Camera stopped working ({str(out)})')
